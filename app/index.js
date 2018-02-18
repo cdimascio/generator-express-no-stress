eca8cf9698d20404860cd0c915e783908245ee3a
Carmine DiMascio <dimascio@us.ibm.com>
2018-02-17 20:19:37 -0500

    #10 When specifying the api root during the generator prompts, if the root / is left off, the swagger doc doesnt load

--- app/index.js
+++ app/index.js
@@ -8,66 +8,66 @@ module.exports = class extends Generator {
     super(args, opts);
     this.argument('appname', { type: String, required: false });
     this.option('yarn', {
-      description: 'Use Yarn as the package manager'
+      description: 'Use Yarn as the package manager',
     });
     this.option('docker', {
-      description: 'Install Docker artifacts including a Dockerfile'
+      description: 'Install Docker artifacts including a Dockerfile',
     });
 
     this.useYarn = this.options.yarn;
     this.docker = this.options.docker;
     this.name = this.options.appname || 'myapp';
-    this.description = 'My cool app'
-    this.version = '1.0.0'
-    this.apiRoot = '/api/v1'
+    this.description = 'My cool app';
+    this.version = '1.0.0';
+    this.apiRoot = '/api/v1';
   }
 
-  initializing() {
-  }
+  initializing() {}
 
   prompting() {
-    const prompts = [{
-      type: 'input',
-      name: 'description',
-      message: `App description [${this.description}]`
-    },{
-      type: 'input',
-      name: 'apiRoot',
-      message: `API Root [${this.apiRoot}]`
-    },{
-      type: 'input',
-      name: 'apiVersion',
-      message: `Version [${this.version}]`
-    }];
+    const prompts = [
+      {
+        type: 'input',
+        name: 'description',
+        message: `App description [${this.description}]`,
+      },
+      {
+        type: 'input',
+        name: 'apiRoot',
+        message: `API Root [${this.apiRoot}]`,
+      },
+      {
+        type: 'input',
+        name: 'apiVersion',
+        message: `Version [${this.version}]`,
+      },
+    ];
 
     if (!this.options.appname) {
       prompts.unshift({
         type: 'input',
         name: 'name',
-        message: `App name [${this.name}]`
-      })
+        message: `App name [${this.name}]`,
+      });
     }
 
-    return this.prompt(prompts)
-      .then(r => {
-        this.name = r.name ? r.name : this.name;
-        this.description = r.description ? r.description : this.description;
-        this.version = r.version ? r.version : this.version;
-        this.apiRoot = r.apiRoot ? r.apiRoot : this.apiRoot;
-      });
+    return this.prompt(prompts).then(r => {
+      this.name = r.name ? r.name : this.name;
+      this.description = r.description ? r.description : this.description;
+      this.version = r.version ? r.version : this.version;
+      this.apiRoot = r.apiRoot ? r.apiRoot.replace(/^\/?/, '/') : this.apiRoot;
+    });
   }
 
-  configuring() {
-  }
+  configuring() {}
 
-  default() {
-  }
+  default() {}
 
   get writing() {
     return {
       appStaticFiles() {
-        const src = this.sourceRoot()
-        const dest = this.destinationPath(this.name)
+        const src = this.sourceRoot();
+        const dest = this.destinationPath(this.name);
 
         const files = [
           'package.json',
@@ -79,47 +79,44 @@ module.exports = class extends Generator {
           'public/api-explorer/index.html',
           'public/api-explorer/swagger-ui-standalone-preset.js',
           'public/index.html',
-          'tgitignore'
+          'tgitignore',
         ];
 
-        const copyOpts = this.docker 
-          ? null 
-          : { 
-          globOptions: {
-            ignore: '**/+(Dockerfile|.dockerignore)'
-          }
-        };
-        this.fs.copy(src, dest, copyOpts)
-        this.fs.copy(
-          this.templatePath('.*'),
-          dest, copyOpts
-        );
+        const copyOpts = this.docker
+          ? null
+          : {
+              globOptions: {
+                ignore: '**/+(Dockerfile|.dockerignore)',
+              },
+            };
+        this.fs.copy(src, dest, copyOpts);
+        this.fs.copy(this.templatePath('.*'), dest, copyOpts);
 
         const opts = {
-            name: this.name,
-            title: this.name,
-            description: this.description,
-            version: this.version,
-            apiRoot: this.apiRoot
-        }
-
+          name: this.name,
+          title: this.name,
+          description: this.description,
+          version: this.version,
+          apiRoot: this.apiRoot,
+        };
 
         files.forEach(f => {
           this.fs.copyTpl(
             this.templatePath(f),
-            this.destinationPath(`${this.name}/${f}`), opts);
+            this.destinationPath(`${this.name}/${f}`),
+            opts,
+          );
         });
 
         this.fs.move(
           this.destinationPath(`${this.name}`, 'tgitignore'),
-          this.destinationPath(`${this.name}`, '.gitignore')
+          this.destinationPath(`${this.name}`, '.gitignore'),
         );
-      }
-    }
+      },
+    };
   }
 
-  conflicts() {
-  }
+  conflicts() {}
 
   install() {
     const appDir = path.join(process.cwd(), this.name);
@@ -131,6 +128,5 @@ module.exports = class extends Generator {
     }
   }
 
-  end() {
-  }
-}
+  end() {}
+};
