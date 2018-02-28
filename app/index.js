16df2581ee073473ade5d6978f4eb03b32685d98
Carmine DiMascio <dimascio@us.ibm.com>
2018-02-28 15:48:36 -0500

    remove trailing commas

--- app/index.js
+++ app/index.js
@@ -104,13 +104,13 @@ module.exports = class extends Generator {
           this.fs.copyTpl(
             this.templatePath(f),
             this.destinationPath(`${this.name}/${f}`),
-            opts,
+            opts
           );
         });
 
         this.fs.move(
           this.destinationPath(`${this.name}`, 'tgitignore'),
-          this.destinationPath(`${this.name}`, '.gitignore'),
+          this.destinationPath(`${this.name}`, '.gitignore')
         );
       },
     };
