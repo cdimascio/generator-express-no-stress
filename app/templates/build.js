23df04a860d9050a6077da409e44ebe5401ac0e2
Carmine DiMascio <dimascio@us.ibm.com>
2018-06-06 17:22:11 -0400

    Auto restart when swagger yaml is modified #18

--- app/templates/build.js
+++ app/templates/build.js
@@ -4,5 +4,3 @@ s.rm('-rf', 'build');
 s.mkdir('build');
 s.cp('.env', 'build/.env');
 s.cp('-R', 'public', 'build/public');
-s.mkdir('-p', 'build/server/common/swagger');
-s.cp('server/common/swagger/Api.yaml', 'build/server/common/swagger/Api.yaml');
