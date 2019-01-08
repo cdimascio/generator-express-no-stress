a9ea81f77cd328a8356721f0e536ae69dc2f0d2d
Carmine DiMascio <dimascio@us.ibm.com>
2019-01-08 14:07:47 -0500

    fix request limit configuration

--- app/templates/server/common/server.js
+++ app/templates/server/common/server.js
@@ -13,8 +13,8 @@ export default class ExpressServer {
   constructor() {
     const root = path.normalize(`${__dirname}/../..`);
     app.set('appPath', `${root}client`);
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || '100kb' }));
+    app.use(bodyParser.urlencoded({ extended: true, limit: process.env.REQUEST_LIMIT || '100kb' }));
     app.use(cookieParser(process.env.SESSION_SECRET));
     app.use(Express.static(`${root}/public`));
   }
