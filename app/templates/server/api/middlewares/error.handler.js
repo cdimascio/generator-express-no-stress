c08b34d3a183d9c253496cea343996951f75af79
Carmine DiMascio <cdimascio@gmail.com>
2019-04-04 16:55:38 -0400

    Update error.handler.js

--- app/templates/server/api/middlewares/error.handler.js
+++ app/templates/server/api/middlewares/error.handler.js
@@ -1,5 +1,4 @@
 <% if (specification === 'openapi_3') { %>
-// Error handler to display the error as HTML
 // eslint-disable-next-line no-unused-vars, no-shadow
 export default function errorHandler(err, req, res, next) {
   const errors = err.errors || [{ message: err.message }];
