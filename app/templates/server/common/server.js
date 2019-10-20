786b8679358d1c9e8a2cd155d28b180e8cc0bf5d
Frank Calise <fcalise@vitralogy.com>
2019-10-20 11:55:04 -0400

    Adding default body parser for text

--- app/templates/server/common/server.js
+++ app/templates/server/common/server.js
@@ -20,6 +20,7 @@ export default class ExpressServer {
     app.set('appPath', `${root}client`);
     app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || '100kb' }));
     app.use(bodyParser.urlencoded({ extended: true, limit: process.env.REQUEST_LIMIT || '100kb' }));
+    app.use(bodyParser.text({ limit: process.env.REQUEST_LIMIT || '100kb'}))
     app.use(cookieParser(process.env.SESSION_SECRET));
     app.use(Express.static(`${root}/public`));
 
