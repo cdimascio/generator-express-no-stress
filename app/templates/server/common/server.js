39093602e3f682c4a9c978a7482c96769399cfee
Carmine DiMascio <cdimascio@gmail.com>
2020-10-10 16:47:34 -0400

    implement support for v4 express openapi validator

--- app/templates/server/common/server.js
+++ app/templates/server/common/server.js
@@ -1,33 +1,54 @@
 import Express from 'express';
+import cookieParser from 'cookie-parser';
 import * as path from 'path';
 import * as bodyParser from 'body-parser';
 import * as http from 'http';
 import * as os from 'os';
-import cookieParser from 'cookie-parser';
-<% if (specification === 'openapi_3') { %>
-import oas from './oas';
+import l from './logger';<% if (specification === 'openapi_3') { %>
+import * as OpenApiValidator from 'express-openapi-validator';
+import errorHandler from '../api/middlewares/error.handler'
 <% } else { %>
 import oas from './swagger';
 <% } %>
-import l from './logger';
-
 const app = new Express();
-const exit = process.exit;
 
 export default class ExpressServer {
   constructor() {
     const root = path.normalize(`${__dirname}/../..`);
+<% if (specification === 'openapi_3') { %>
+    const apiSpec = path.join(__dirname, 'api.yml');
+    const validateResponses = !!(
+      process.env.OPENAPI_ENABLE_RESPONSE_VALIDATION &&
+      process.env.OPENAPI_ENABLE_RESPONSE_VALIDATION.toLowerCase() === 'true'
+    );
+<% } %>
     app.set('appPath', `${root}client`);
     app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || '100kb' }));
     app.use(bodyParser.urlencoded({ extended: true, limit: process.env.REQUEST_LIMIT || '100kb' }));
     app.use(bodyParser.text({ limit: process.env.REQUEST_LIMIT || '100kb'}));
     app.use(cookieParser(process.env.SESSION_SECRET));
     app.use(Express.static(`${root}/public`));
+<% if (specification === 'openapi_3') { %>
+    app.use(process.env.OPENAPI_SPEC || '/spec', Express.static(apiSpec));
+    app.use(
+      OpenApiValidator.middleware({
+        apiSpec,
+        validateResponses,
+        ignorePaths: /.*\/spec(\/|$)/,
+      })
+    );
+<% } %>
   }
 
   router(routes) {
+<% if (specification === 'openapi_3') { %>
+    routes(app)
+    app.use(errorHandler)
+    return this;
+<% } else { %>
     this.routes = routes;
     return this;
+<% } %>
   }
 
   listen(port = process.env.PORT) {
@@ -36,14 +57,17 @@ export default class ExpressServer {
         `up and running in ${process.env.NODE_ENV ||
           'development'} @: ${os.hostname()} on port: ${p}}`
       );
-
+<% if (specification === 'openapi_3') { %>
+    http.createServer(app).listen(port, welcome(port));
+<% } else { %>
     oas(app, this.routes).then(() => {
       http.createServer(app).listen(port, welcome(port));
     }).catch(e => {
       l.error(e);
-      exit(1)
+      // eslint-disable-next-line no-process-exit
+      process.exit(1)
     });
-
+<% } %>
     return app;
   }
 }
